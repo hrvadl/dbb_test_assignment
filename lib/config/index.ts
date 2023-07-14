d984ca1744278ed1729cb14484828a9d8ff8a97e
hrvadl <youngwwad@gmail.com>
2023-07-14 17:33:57 +0300

    added dotenv file & upgraded configuration

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,5 +1,6 @@
+import { REACT_APP_DROPBOX_API, REACT_APP_DROPBOX_TOKEN } from '@env'
+
 export const config = {
-  baseURL: 'https://api.dropboxapi.com/2',
-  token:
-    'sl.BiJ90w3w17xf8vrrSfKWqou_XSeOLGsu1GzBBsOQKi96pxGinHZbXOPQoOP17cxa85OIcp_se4LSgN4N0E_TaCuGHPvj0OEA9yaembpln26W_OVBF7Lv_gfsxwAnoFPyPx9uKKc5',
+  baseURL: REACT_APP_DROPBOX_API ?? '',
+  token: REACT_APP_DROPBOX_TOKEN ?? '',
 }
